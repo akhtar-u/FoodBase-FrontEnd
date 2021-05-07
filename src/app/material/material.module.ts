e11e2fb097e711f07b642a216b7afa8d67457497
Usman Akhtar <akhtar_usman@outlook.com>
2021-05-06 23:25:33 -0400

    added icons to toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatIconModule
 ];
 
 @NgModule({
