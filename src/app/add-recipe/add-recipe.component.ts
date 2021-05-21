acae28fb7fdc7e725b4e5e90da2a901515dc320d
Usman Akhtar <akhtar_usman@outlook.com>
2021-05-20 21:57:15 -0400

    finish instructions and ingredients form layout

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 
 @Component({
@@ -14,8 +14,8 @@ export class AddRecipeComponent implements OnInit {
 
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
-  thirdFormGroup!: FormGroup;
-  fourthFormGroup!: FormGroup;
+  ingredientsFormGroup!: FormGroup;
+  instructionsFormGroup!: FormGroup;
   fifthFormGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
@@ -31,6 +31,44 @@ export class AddRecipeComponent implements OnInit {
     this.fifthFormGroup = this.formBuilder.group({
       fifthCtrl: ['', Validators.required]
     });
+    this.ingredientsFormGroup = this.formBuilder.group({
+      ingredientsCtrl: ['', Validators.required],
+      ingredients: this.formBuilder.array([])
+    });
+    this.instructionsFormGroup = this.formBuilder.group({
+      instructionsCtrl: ['', Validators.required],
+      instructions: this.formBuilder.array([])
+    });
   }
+
+  addIngredient(): void {
+    (this.ingredientsFormGroup.get('ingredients') as FormArray).push(this.formBuilder.group({
+      ingredient: []
+    }));
+  }
+
+  addInstruction(): void {
+    (this.instructionsFormGroup.get('instructions') as FormArray).push(this.formBuilder.group({
+      instruction: []
+    }));
+  }
+
+  removeIngredient(i: number): void {
+    (this.ingredientsFormGroup.get('ingredients') as FormArray).removeAt(i);
+  }
+
+  removeInstruction(i: number): void {
+    (this.instructionsFormGroup.get('instructions') as FormArray).removeAt(i);
+  }
+
+  get ingredients(): AbstractControl[] {
+    return (this.ingredientsFormGroup.get('ingredients') as FormArray).controls;
+  }
+
+  get instructions(): AbstractControl[] {
+    return (this.instructionsFormGroup.get('instructions') as FormArray).controls;
+  }
+
+
 
 }
