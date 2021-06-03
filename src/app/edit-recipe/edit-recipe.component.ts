e231b58c1c898fe355e990a3a621db42e778842f
Usman Akhtar <akhtar_usman@outlook.com>
2021-06-03 02:01:38 -0400

    finish edit recipe ui

--- src/app/edit-recipe/edit-recipe.component.ts
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-edit-recipe',
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditRecipeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  fileAttr = '';
+  dataImage: any;
+  imageToggleVal!: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  uploadFileEvt(imgFile: any): void {
+    if (imgFile.target.files && imgFile.target.files[0]) {
+      this.fileAttr = '';
+      // @ts-ignore
+      Array.from(imgFile.target.files).forEach((file: File) => {
+        this.fileAttr += file.name;
+      });
+
+      // HTML5 FileReader API
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = rs => {
+          this.dataImage = e.target.result;
+        };
+      };
+      reader.readAsDataURL(imgFile.target.files[0]);
+
+      // Reset if duplicate image uploaded again
+      this.fileInput.nativeElement.value = '';
+    }
+    else {
+      this.fileAttr = '';
+    }
+  }
+
+  showPreview(event: any): void {
+    this.dataImage = event.target.value;
+  }
+
+  onValChange(value: any): void {
+    this.imageToggleVal = value;
+    this.dataImage = null;
   }
 
 }
